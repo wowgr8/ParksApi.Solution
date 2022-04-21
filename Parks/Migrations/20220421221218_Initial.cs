ce287a153c013ac3b14ea8a4b7ce50c2731be0c4
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-21 15:13:25 -0700

    add Initial migration

---
+++ Parks/Migrations/20220421221218_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parks.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StateAndNatParks",
+                columns: table => new
+                {
+                    StateAndNatParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Type = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    LocationByState = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StateAndNatParks", x => x.StateAndNatParkId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StateAndNatParks");
+        }
+    }
+}
