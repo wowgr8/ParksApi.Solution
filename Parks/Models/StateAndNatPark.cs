a258345a25b4a4ed6300a573e7e8aba9c7ef3b96
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-21 14:47:59 -0700

    add model, class, and properties

---
+++ Parks/Models/StateAndNatPark.cs
@@ -0,0 +1,10 @@
+namespace Parks.Models
+{
+  public class StateAndNatPark
+  {
+    public int StateAndNatParkId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string LocationByState { get; set; }
+  }
+}
