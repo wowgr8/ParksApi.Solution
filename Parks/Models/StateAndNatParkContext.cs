3bb90cf26ae9d9bc84f8ac63c22c098a7245ab34
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-21 19:22:24 -0700

    add 'OnModelCreating' protected override method with data for Seeding Data

--- Parks/Models/StateAndNatParkContext.cs
+++ Parks/Models/StateAndNatParkContext.cs
@@ -9,6 +9,19 @@ namespace Parks.Models
     {
     }
 
-      public DbSet<StateAndNatPark> StateAndNatParks { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<StateAndNatPark>()
+        .HasData(
+          new StateAndNatPark { StateAndNatParkId = 1, Name = "Yosimite", Type = "National Park", LocationByState = "California" },
+          new StateAndNatPark { StateAndNatParkId = 2, Name = "Yellowstone", Type = "National Park", LocationByState = "Wyoming" },
+          new StateAndNatPark { StateAndNatParkId = 3, Name = "Arches", Type = "National Park", LocationByState = "Utah" },
+          new StateAndNatPark { StateAndNatParkId = 4, Name = "Palo Duro Canyon", Type = "State Park", LocationByState = "Texas" },
+          new StateAndNatPark { StateAndNatParkId = 5, Name = "Chimney Rock", Type = "State Park", LocationByState = "North Carolina" },
+          new StateAndNatPark { StateAndNatParkId = 6, Name = "Porcupine Mountains Wilderness", Type = "State Park", LocationByState = "Michigan" }
+        );
+    }
+
+    public DbSet<StateAndNatPark> StateAndNatParks { get; set; }
   }
 }
