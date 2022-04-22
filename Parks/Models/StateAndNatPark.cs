4c0ab899cdc700294a7ccea8e7ccbb4e86aa8765
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-21 23:31:48 -0700

    add using directive and Validation requirements

--- Parks/Models/StateAndNatPark.cs
+++ Parks/Models/StateAndNatPark.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Parks.Models
 {
   public class StateAndNatPark
   {
     public int StateAndNatParkId { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
+    [StringLength(13, ErrorMessage = "Type must be 'National park' or 'State Park'.")]
     public string Type { get; set; }
+    [Required]
     public string LocationByState { get; set; }
   }
 }
